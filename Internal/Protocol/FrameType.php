eae6f9796fde7261e8943d84567a096bf88963ca
kafkiansky <vadimzanfir@gmail.com>
2024-12-25 18:12:58 +0300

    Implement simple Buffer

--- Internal/Protocol/FrameType.php
+++ Internal/Protocol/FrameType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Typhoon\Amqp091\Internal\Protocol;
 
+use Typhoon\ByteOrder\ReadFrom;
+
 /**
  * @internal
  * @psalm-internal Typhoon\Amqp091
@@ -14,4 +16,9 @@ enum Frame: int
     case header = 2;
     case body = 3;
     case heartbeat = 8;
+
+    public function parse(ReadFrom $reader)
+    {
+        //
+    }
 }
