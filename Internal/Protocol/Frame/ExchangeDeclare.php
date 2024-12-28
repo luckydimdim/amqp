c924418c9f37e54cf1dab11966338bb343a343b1
kafkiansky <vadimzanfir@gmail.com>
2024-12-28 19:52:53 +0300

    Implement exchange.declare.

---
+++ Internal/Protocol/Frame/ExchangeDeclare.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Amqp091\Internal\Protocol\Frame;
+
+use Typhoon\Amqp091\Internal\Io;
+use Typhoon\Amqp091\Internal\Protocol\Frame;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Amqp091
+ */
+final class ExchangeDeclare implements Frame
+{
+    /**
+     * @param non-empty-string $exchange
+     * @param non-empty-string $exchangeType
+     * @param array<string, mixed> $arguments
+     */
+    public function __construct(
+        public readonly string $exchange,
+        public readonly string $exchangeType,
+        public readonly bool $passive = false,
+        public readonly bool $durable = false,
+        public readonly bool $autoDelete = false,
+        public readonly bool $internal = false,
+        public readonly bool $noWait = false,
+        public readonly array $arguments = [],
+        public readonly int $reserved1 = 0,
+    ) {}
+
+    public static function read(Io\ReadBytes $reader): Frame
+    {
+        $reserved1 = $reader->readUint16();
+        $exchange = $reader->readString();
+        $exchangeType = $reader->readString();
+        [$passive, $durable, $autoDelete, $internal, $noWait] = $reader->readBits(5);
+        $arguments = $reader->readTable();
+
+        return new self(
+            $exchange,
+            $exchangeType,
+            $passive,
+            $durable,
+            $autoDelete,
+            $internal,
+            $noWait,
+            $arguments,
+            $reserved1,
+        );
+    }
+
+    public function write(Io\WriteBytes $writer): Io\WriteBytes
+    {
+        return $writer
+            ->writeUint16($this->reserved1)
+            ->writeString($this->exchange)
+            ->writeString($this->exchangeType)
+            ->writeBits($this->passive, $this->durable, $this->autoDelete, $this->internal, $this->noWait)
+            ->writeTable($this->arguments);
+    }
+}
