a7ffd480c0bdb483e73b93cb475c5ee8674b7ef9
kafkiansky <vadimzanfir@gmail.com>
2024-12-28 19:53:28 +0300

    Implement protocol header.

---
+++ Internal/Protocol/Frame/ProtocolHeader.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Amqp091\Internal\Protocol\Frame;
+
+use Typhoon\Amqp091\Internal\Protocol\Frame;
+use Typhoon\Amqp091\Internal\Io;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Amqp091
+ */
+enum ProtocolHeader implements Frame
+{
+    case frame;
+
+    public static function read(Io\ReadBytes $reader): self
+    {
+        // len('AMQP') + 4 * len(uint8).
+        $reader->discard(8);
+
+        return self::frame;
+    }
+
+    public function write(Io\WriteBytes $writer): Io\WriteBytes
+    {
+        return $writer
+            ->write('AMQP')
+            ->writeUint8(0)
+            ->writeUint8(0)
+            ->writeUint8(9)
+            ->writeUint8(1);
+    }
+}
