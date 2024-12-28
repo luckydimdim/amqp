6e25c2fdf3208f11ce0f5e05714acfcf124c34e9
kafkiansky <vadimzanfir@gmail.com>
2024-12-28 19:53:17 +0300

    Implement queue.declare.

---
+++ Internal/Protocol/Frame/QueueDeclare.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Amqp091\Internal\Protocol\Frame;
+
+use Typhoon\Amqp091\Internal\Io;
+use Typhoon\Amqp091\Internal\Protocol\Frame;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Amqp091
+ */
+final class QueueDeclare implements Frame
+{
+    /**
+     * @param array<string, mixed> $arguments
+     */
+    public function __construct(
+        public readonly string $queue,
+        public readonly bool $passive = false,
+        public readonly bool $durable = false,
+        public readonly bool $exclusive = false,
+        public readonly bool $autoDelete = false,
+        public readonly bool $noWait = false,
+        public readonly array $arguments = [],
+        public readonly int $reserved1 = 0,
+    ) {}
+
+    public static function read(Io\ReadBytes $reader): self
+    {
+        $reserved1 = $reader->readUint16();
+        $queue = $reader->readString();
+        [$passive, $durable, $exclusive, $autoDelete, $noWait] = $reader->readBits(5);
+        $arguments = $reader->readTable();
+
+        return new self(
+            $queue,
+            $passive,
+            $durable,
+            $exclusive,
+            $autoDelete,
+            $noWait,
+            $arguments,
+            $reserved1,
+        );
+    }
+
+    public function write(Io\WriteBytes $writer): Io\WriteBytes
+    {
+        return $writer
+            ->writeUint16(0)
+            ->writeString($this->queue)
+            ->writeBits($this->passive, $this->durable, $this->exclusive, $this->autoDelete, $this->noWait)
+            ->writeTable($this->arguments);
+    }
+}
---
+++ Internal/Protocol/Frame/QueueDeclareOk.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Amqp091\Internal\Protocol\Frame;
+
+use Typhoon\Amqp091\Internal\Protocol\Frame;
+use Typhoon\Amqp091\Internal\Io;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Amqp091
+ */
+final class QueueDeclareOk implements Frame
+{
+    /**
+     * @param non-empty-string $queue
+     * @param non-negative-int $messages
+     * @param non-negative-int $consumers
+     */
+    public function __construct(
+        public readonly string $queue,
+        public readonly int $messages,
+        public readonly int $consumers,
+    ) {}
+
+    public static function read(Io\ReadBytes $reader): self
+    {
+        /** @var non-empty-string $queue */
+        $queue = $reader->readString();
+
+        /** @var non-negative-int $messages */
+        $messages = $reader->readInt32();
+
+        /** @var non-negative-int $consumers */
+        $consumers = $reader->readInt32();
+
+        return new self(
+            $queue,
+            $messages,
+            $consumers,
+        );
+    }
+
+    public function write(Io\WriteBytes $writer): Io\WriteBytes
+    {
+        return $writer
+            ->writeString($this->queue)
+            ->writeInt32($this->messages)
+            ->writeInt32($this->consumers);
+    }
+}
