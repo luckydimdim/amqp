cbbd519c690ce38e0903db9deea0674c5a7f4068
kafkiansky <vadimzanfir@gmail.com>
2024-12-28 19:52:43 +0300

    Implement connection.tune.

---
+++ Internal/Protocol/Frame/ConnectionTune.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Amqp091\Internal\Protocol\Frame;
+
+use Typhoon\Amqp091\Internal\Protocol\Frame;
+use Typhoon\Amqp091\Internal\Io;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Amqp091
+ */
+final class ConnectionTune implements Frame
+{
+    public function __construct(
+        public readonly int $channelMax,
+        public readonly int $frameMax,
+        public readonly int $heartbeat,
+    ) {}
+
+    public static function read(Io\ReadBytes $reader): self
+    {
+        return new self(
+            $reader->readInt16(),
+            $reader->readInt32(),
+            $reader->readInt16(),
+        );
+    }
+
+    public function write(Io\WriteBytes $writer): Io\WriteBytes
+    {
+        return $writer
+            ->writeInt16($this->channelMax)
+            ->writeInt32($this->frameMax)
+            ->writeInt16($this->heartbeat);
+    }
+}
---
+++ Internal/Protocol/Frame/ConnectionTuneOk.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Amqp091\Internal\Protocol\Frame;
+
+use Typhoon\Amqp091\Internal\Io;
+use Typhoon\Amqp091\Internal\Protocol\Frame;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Amqp091
+ */
+final class ConnectionTuneOk implements Frame
+{
+    /**
+     * @param non-negative-int $channelMax
+     * @param non-negative-int $frameMax
+     * @param non-negative-int $heartbeat
+     */
+    public function __construct(
+        public readonly int $channelMax,
+        public readonly int $frameMax,
+        public readonly int $heartbeat,
+    ) {}
+
+    public static function read(Io\ReadBytes $reader): self
+    {
+        return new self(
+            $reader->readUint16(),
+            $reader->readUint32(),
+            $reader->readUint16(),
+        );
+    }
+
+    public function write(Io\WriteBytes $writer): Io\WriteBytes
+    {
+        return $writer
+            ->writeUint16($this->channelMax)
+            ->writeUint32($this->frameMax)
+            ->writeUint16($this->heartbeat);
+    }
+}
