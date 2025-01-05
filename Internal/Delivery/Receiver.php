aa50e24689bc9282bd9fdd4da7b38135f29d1922
kafkiansky <vadimzanfir@gmail.com>
2025-01-05 16:35:51 +0300

    DeliverySupervisor and Receiver implemented

--- Internal/Delivery/Receiver.php
+++ Internal/Delivery/Receiver.php
@@ -4,160 +4,52 @@ declare(strict_types=1);
 
 namespace Typhoon\Amqp091\Internal\Delivery;
 
-use Typhoon\Amqp091\Channel;
+use Amp\Pipeline;
 use Typhoon\Amqp091\Delivery;
-use Typhoon\Amqp091\Internal\Hooks;
-use Typhoon\Amqp091\Internal\Protocol\Frame;
 
 /**
  * @internal
  * @psalm-internal Typhoon\Amqp091
- * @psalm-type Listener = callable(Delivery): void
  */
 final class Receiver
 {
-    private const WAIT = 0;
-    private const HEADER = 1;
-    private const BODY = 2;
-
-    /** @var self::* */
-    private int $step = self::WAIT;
-
-    private ?Frame\BasicDeliver $delivery = null;
-
-    private ?Frame\BasicReturn $return = null;
-
-    private ?Frame\ContentHeader $header = null;
-
-    /** @var non-negative-int */
-    private int $messageSize = 0;
-
-    private string $message = '';
-
-    /** @var list<Listener> */
-    private array $listeners = [];
-
-    /**
-     * @param non-negative-int $channelId
-     */
-    public function __construct(
-        private readonly Channel $channel,
-        private readonly Hooks $hooks,
-        private readonly int $channelId,
-    ) {}
-
-    public function run(): void
+    public static function create(DeliverySupervisor $supervisor): self
     {
-        $this->subscribe(Frame\BasicDeliver::class, $this->onBasicDeliver(...));
-        $this->subscribe(Frame\BasicReturn::class, $this->onBasicReturn(...));
-        $this->subscribe(Frame\ContentHeader::class, $this->onContentHeader(...));
-        $this->subscribe(Frame\ContentBody::class, $this->onContentBody(...));
+        $receiver = new self($supervisor);
+        $receiver->run();
+
+        return $receiver;
     }
 
-    /**
-     * @param Listener $callback
-     */
-    public function addListener(callable $callback): void
+    public function receive(): ?Delivery
     {
-        $this->listeners[] = $callback;
-    }
-
-    public function stop(): void
-    {
-        $this->listeners = [];
-    }
-
-    private function onBasicDeliver(Frame\BasicDeliver $delivery): void
-    {
-        if ($this->step === self::WAIT) {
-            [$this->delivery, $this->step] = [$delivery, self::HEADER];
-        }
-    }
-
-    private function onBasicReturn(Frame\BasicReturn $return): void
-    {
-        if ($this->step === self::WAIT) {
-            [$this->return, $this->step] = [$return, self::HEADER];
-        }
-    }
-
-    private function onContentHeader(Frame\ContentHeader $header): void
-    {
-        if ($this->step === self::HEADER) {
-            $this->header = $header;
-            $this->step = self::BODY;
-            $this->messageSize = $this->header->bodySize;
-
-            $this->runListeners();
-        }
-    }
-
-    private function onContentBody(Frame\ContentBody $body): void
-    {
-        if ($this->step === self::BODY) {
-            $this->message .= $body->body;
-            $this->messageSize = max($this->messageSize - \strlen($body->body), 0);
-
-            $this->runListeners();
-        }
-    }
-
-    private function runListeners(): void
-    {
-        if ($this->messageSize !== 0) {
-            return;
+        if (!$this->iterator->continue()) {
+            return null;
         }
 
-        \assert($this->delivery !== null || $this->return !== null, 'delivery or return must not be empty.');
-        \assert($this->header !== null, 'header must not be empty.');
-
-        // You cannot call ack/nack/reject on a returned message.
-        $noAction = static function (): void {};
-
-        $delivery = new Delivery(
-            ack: $this->return !== null ? $noAction : $this->channel->ack(...),
-            nack: $this->return !== null ? $noAction : $this->channel->nack(...),
-            reject: $this->return !== null ? $noAction : $this->channel->reject(...),
-            body: $this->message,
-            exchange: $this->delivery?->exchange ?? $this->return?->exchange ?? '',
-            routingKey: $this->delivery?->routingKey ?? $this->return?->routingKey ?? '',
-            headers: $this->header->properties->headers,
-            deliveryTag: $this->delivery?->deliveryTag ?? 0,
-            consumerTag: $this->delivery?->consumerTag ?? '',
-            redelivered: $this->delivery?->redelivered ?? false,
-            contentType: $this->header->properties->contentType,
-            contentEncoding: $this->header->properties->contentEncoding,
-            deliveryMode: $this->header->properties->deliveryMode,
-            priority: $this->header->properties->priority,
-            correlationId: $this->header->properties->correlationId,
-            replyTo: $this->header->properties->replyTo,
-            expiration: $this->header->properties->expiration,
-            messageId: $this->header->properties->messageId,
-            timestamp: $this->header->properties->timestamp,
-            type: $this->header->properties->type,
-            userId: $this->header->properties->userId,
-            appId: $this->header->properties->appId,
-            returned: $this->return !== null,
-        );
-
-        foreach ($this->listeners as $listener) {
-            $listener($delivery);
-        }
-
-        $this->delivery = null;
-        $this->return = null;
-        $this->header = null;
-        $this->message = '';
-        $this->step = self::WAIT;
+        return $this->iterator->getValue();
     }
 
-    /**
-     * @template T of Frame
-     * @param class-string<T> $frameType
-     * @param \Closure(T): void $callback
-     */
-    private function subscribe(string $frameType, \Closure $callback): void
+    /** @var Pipeline\ConcurrentIterator<null|Delivery> */
+    private Pipeline\ConcurrentIterator $iterator;
+
+    /** @var Pipeline\Queue<null|Delivery> */
+    private Pipeline\Queue $queue;
+
+    private function __construct(
+        private readonly DeliverySupervisor $supervisor,
+    ) {
+        /** @var Pipeline\Queue<null|Delivery> $queue */
+        $queue = new Pipeline\Queue(bufferSize: 1);
+
+        $this->queue = $queue;
+        $this->iterator = $queue->iterate();
+    }
+
+    private function run(): void
     {
-        $this->hooks->subscribe($this->channelId, $frameType, $callback);
+        /** @psalm-suppress InvalidArgument https://github.com/vimeo/psalm/issues/4589 */
+        $this->supervisor->addGetListener($this->queue->push(...));
+        $this->supervisor->addShutdownListener($this->queue->complete(...));
     }
 }
