aecf33d4bb25dd10e8607d7ce49370d41bd77b40
kafkiansky <vadimzanfir@gmail.com>
2025-04-14 07:59:31 +0300

    Allow concurrent and exclusive call to `DeliveryMessage::ack`, `DeliveryMessage::nack` or `DeliveryMessage::reject` (#23)
    * Allow concurrent and exclusive call to `Delivery::ack`, `Delivery::nack` or `Delivery::reject`.
    
    * Add concurrency in DeliveryMessageTest.

---
+++ tests/DeliveryMessageTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Amqp;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\TestWith;
+use PHPUnit\Framework\TestCase;
+use function Amp\async;
+use function Amp\Future\awaitAll;
+
+#[CoversClass(DeliveryMessage::class)]
+final class DeliveryMessageTest extends TestCase
+{
+    /**
+     * @param 'ack'|'nack'|'reject' $operationType
+     */
+    #[TestWith(['ack'])]
+    #[TestWith(['nack'])]
+    #[TestWith(['reject'])]
+    public function testRepetitiveOperation(string $operationType): void
+    {
+        $count = 0;
+        $operation = static function () use (&$count): void {
+            ++$count;
+        };
+
+        $delivery = new DeliveryMessage(
+            ack: $operation,
+            nack: $operation,
+            reject: $operation,
+            message: new Message('x'),
+        );
+
+        $call = match ($operationType) {
+            'ack' => $delivery->ack(...),
+            'nack' => $delivery->nack(...),
+            'reject' => $delivery->reject(...),
+        };
+
+        $futures = [];
+
+        for ($i = 0; $i < 10; ++$i) {
+            $futures[] = async($call);
+        }
+
+        awaitAll($futures);
+
+        self::assertSame(1, $count);
+    }
+}
